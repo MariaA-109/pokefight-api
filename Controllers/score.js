d170c975918758ecdafa8c700f3f4fc9d42e6911
MariaA-109 <122259443+MariaA-109@users.noreply.github.com>
2023-06-06 14:58:04 +0200

    added .js

--- Controllers/score.js
+++ Controllers/score.js
@@ -1,35 +1,28 @@
-const Score = require("../model/score");
-
-
+const Score = require("../model/score.js");
 
 const getAllScores = async (req, res) => {
-    try {
-        console.log(req.body);
-        const score = await Score.find();
-        res.status(200). json(score)
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send(err.message)
-    }
-}
-
-
+  try {
+    console.log(req.body);
+    const score = await Score.find();
+    res.status(200).json(score);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
+};
 
 const createScore = async (req, res) => {
-    try {
-        const { first_pokemon, second_pokemon, winner, total_wins } = req.body;
-        const newScore = await Score.create(req.body);
-        res.status(201).json(newScore)
-    }
-    catch (err){
-        console.log(err);
-        res.status(500).send(err.message)
-    }
-}
-
+  try {
+    const { first_pokemon, second_pokemon, winner, total_wins } = req.body;
+    const newScore = await Score.create(req.body);
+    res.status(201).json(newScore);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
+};
 
 module.exports = {
-    getAllScores,
-    createScore
-}
+  getAllScores,
+  createScore,
+};
